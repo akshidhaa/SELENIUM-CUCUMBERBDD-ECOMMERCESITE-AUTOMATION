b5c6f48e64f50226cefda4b371411c7df0d05177
akshidhaa <akshidhaabalaji@gmail.com>
2026-06-22 18:26:53 +0530

    Update ProductPage.java

--- ProductPage.java
+++ ProductPage.java
@@ -21,24 +21,33 @@ By removeBtn=By.xpath("//button[contains(@id,'remove')])[1]");
 By cartIcon=By.className("shopping_cart_link");
 By products=By.className("inventory_item");
 public int getProductCount()
-{
-    return driver.findElements(products).size(0;
-}
-public void addFirstProduct()
-{
-    wait.until(ExpectedConditions.elementToBeClickable(adToCartBtn)).click();
-    wait.until(ExpectedConditions.visibilityOfElementLocated(removeBtn));
-    System.out.println("Product CONFIRMED added");
-}
-public void clickCart()
   {
-    wait.until(ExpectedConditions.elementToBeClickable(cartIcon)).click();
-    try
-      {
-        Thread.sleep(5000);
-      }
-    catch(Exception e)
-      {}
-    System.out.println("Navigated to cart");
-  }
+     List<WebElement> list = driver.findElements(products);
+     return list.size();
+    }
+public void addFirstProduct() {
+  wait.until(ExpectedConditions.elementToBeClickable(addToCartBtn)).click();
+  wait.until(ExpectedConditions.visibilityOfElementLocated(removeBtn));
+  System.out.println("☑️ Product added");
+}
+public void clickCart() {
+
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+
+        int attempts = 0;
+
+        while (!driver.getCurrentUrl().contains("cart")&& attempts < 4) {
+          js.executeScript("arguments[0].click();",driver.findElement(cartIcon));
+          try {
+                Thread.sleep(1000);
+            }
+            catch (Exception e) {}
+
+            attempts++;
+        }
+
+        wait.until(ExpectedConditions.urlContains("cart"));
+
+        System.out.println("☑️ Cart opened: "+ driver.getCurrentUrl());
+    }
 }
